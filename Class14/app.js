d1fd2eb8837780065caa2c26b4909409a0b4cfea
Muhammad Abdullah <116350349+M-Abdullah786@users.noreply.github.com>
2024-09-08 20:32:32 +0500

    Add files via upload

---
+++ Class14/app.js
@@ -0,0 +1,124 @@
+// Chapter 1
+// q1
+alert("Hi, Welcome To Our Services")
+    // q2
+alert("Error! Please enter a valid password")
+    // q3
+alert("Welcome to JS Land... \n Happy Coding!")
+    // q4
+alert("Welcome to JS Land...")
+alert("Happy Coding!")
+    // q5
+alert("Hello.. I can run JS through my web browser's console")
+    // q6
+    // q7
+
+// Chapter 2
+var username
+var myName = "Muhammad Abdullah"
+var message
+message = "Hello World..."
+    // q3
+alert(message)
+    // q4
+alert(myName)
+alert("15 years old")
+alert("Certified Mobile Application Development")
+    // q5
+alert("PIZZA \n PIZZ \n PIZ \n PI \n P")
+    // q6
+var email
+email = "itsmabdullah101@gmail.com"
+alert("My email address is" + " " + email)
+    // q7
+var book
+book = "A smarter way to learn JavaScript"
+alert("I am trying to learn from the Book" + " " + book)
+    // q8
+document.write("Yah! I can write HTML content through Javascript")
+    // q9
+var des_alert = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬"
+alert(des_alert)
+
+
+// Chapter 3
+
+// q1
+var age = 15
+alert("I am " + age + "years old")
+
+// q2
+var visited_num = "You have visited this site 10 times"
+alert(visited_num)
+
+// q3
+var birthYear = 2004
+document.write("My birth year is " + birthYear)
+document.write("Datatype of the decleard variable is " + typeof(birthYear))
+    // q4
+var visi_name = "John Doe"
+var pro_titl = "Shoes"
+var pro_shoe_quant = 5
+
+document.write(visi_name + " ordered " + pro_shoe_quant + pro_titl + " on XYZ Clothing store")
+
+// chapter 4
+
+q1
+let x = 10,
+    y = 20,
+    z = 30;
+
+// q2
+//legal
+var userName;
+var _age;
+var $price;
+var first_name;
+var total5;
+
+//illegal
+ndPlace
+lets
+uuser - name
+@data
+var1
+
+// q3
+document.write("<h1>Rules for naming JS variables</h1>");
+document.write("<p>Variable names can only contain letters, digits, underscores, and dollar signs. For example $my_1stVariable</p>");
+document.write("<p>Variables must begin with a letter, dollar sign ($), or underscore (_). For example $name, _name, or name</p>");
+document.write("<p>Variable names are case sensitive</p>");
+document.write("<p>Variable names should not be JS keywords</p>");
+
+// chapter 1 - 3
+// chapter 1
+//q1
+alert("Muhammad")
+alert("Abdullah")
+alert("itsmyemail@gmail.com")
+alert(+923254785682)
+alert("its a secret")
+    //q2
+alert("YOu're Learning Javascript!")
+    //q3
+alert("You already display too much now its my end")
+
+// chapter 2
+//q1
+var camelCase
+    //q2
+var end
+end = "Why you are asking same thing again and again?"
+    //q3
+var teamName = "Champs"
+alert("My team name is " + teamName)
+    // q4
+var bestMan = "Charlie"
+bestMan = "Myself"
+
+// chapter 3
+// q1-2
+var caseQTY = 144
+    // q3
+var num = "9"
